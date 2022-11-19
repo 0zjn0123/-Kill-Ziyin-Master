a606b979213069b98e26fc18b4afd8aa8cb382f3
小逮逮 <116432703+0zjn0123@users.noreply.github.com>
2022-11-19 21:16:36 +0800

    Create zz.py

---
+++ zz.py
@@ -0,0 +1,6 @@
+import os
+def cc(lj, zzwjj_ml):
+    for path, dirnames, filenames in os.walk(lj):
+        # print("路径：" + path + "包含文件夹：" + str(dirnames) + "包含文件：" + str(filenames))
+        if str(dirnames) or str(filenames) == zzwjj_ml:
+            return {'lj':path,'wjj':dirnames,'wj':filenames}
